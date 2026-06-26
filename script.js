bf4988add4515ed73f2fde2cc820227d100a29f8
lucashiurcartz-jpg <lucas.hiurcartz@escola.pr.gov.br>
2026-06-26 11:03:45 -0300

    ll

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("btn");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", () => {
+    mensagem.innerHTML =
+        "GTA 6 promete ser um dos maiores lançamentos da Rockstar Games, com um enorme mundo aberto, gráficos avançados e novas mecânicas de gameplay.";
+});
